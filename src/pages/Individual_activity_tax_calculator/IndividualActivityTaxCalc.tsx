7d0a8c5697626dfbdb22a4f304627211b66c6b55
Greta Lukošiūnaitė <greta.lukosiunaite@outlook.com>
2023-07-03 21:14:39 +0300

    calling IndividActivTaxCalcualtor component on IndividualActivityTaxCalc.tsx page

--- src/pages/Individual_activity_tax_calculator/IndividualActivityTaxCalc.tsx
+++ src/pages/Individual_activity_tax_calculator/IndividualActivityTaxCalc.tsx
@@ -1,5 +1,9 @@
+import IndividActivTaxCalcualtor from "../../components/organisims/IndividualActivityTaxCalculator";
+
 const IndividualActivityTaxCalc = () => {
-  return <div>IndividualActivityTaxCalc</div>;
+  return <div>IndividualActivityTaxCalc
+    <IndividActivTaxCalcualtor/>
+  </div>;
 };
 
 export default IndividualActivityTaxCalc;

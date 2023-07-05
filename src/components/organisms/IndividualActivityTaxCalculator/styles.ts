9d4701fc3cc7096026a99fce2684149ec6ec04aa
Greta Lukošiūnaitė <greta.lukosiunaite@outlook.com>
2023-07-05 21:23:54 +0300

    In IndividActivTaxCalcualtor component styles.ts added more styled comonents witch are used in IndividActivTaxCalcualtor component

--- src/components/organisms/IndividualActivityTaxCalculator/styles.ts
+++ src/components/organisms/IndividualActivityTaxCalculator/styles.ts
@@ -1,12 +1,55 @@
-import { styled } from "styled-components";
+import { styled } from 'styled-components';
 
-export const StyledTitleRow = styled.div`
-margin-top: 50px;
-display: flex;
-gap: 50px;
-`
+export const StyledContentContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 43px;
+
+  h1 {
+    align-self: center;
+  }
+`;
+
+export const SyledRadioWithLabel = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.25em;
+`;
+
+export const SyledRadioWrapper = styled.div`
+  display: flex;
+  width: 100%;
+  gap: 0.75em;
+`;
+
+
+export const StyledTitleRow1 = styled.div`
+  display: flex;
+  width: calc(100% - 0.5rem);
+  justify-content: space-between;
+  background-color: #ff9a0c;
+  color: white;
+  padding: 0.25rem;
+`;
+
+export const StyledTitleRow2 = styled.div`
+  width: calc(100% - 0.5rem);
+  background-color: #68a5e6;
+  color: white;
+  padding: 0.25rem;
+`;
+
+export const StyledLableColumn = styled.label`
+width: 30%;
+`;
+
 
 export const StyledRow = styled.div`
-display: flex;
-gap: 50px;
-`
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const StyledColumnLable = styled.label`
+width: calc(70% / 3);
+text-align: right;
+`;

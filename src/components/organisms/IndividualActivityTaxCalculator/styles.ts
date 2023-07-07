c29c8f22ff0fa68bad36393f47ebd1a486ff9a62
Greta Lukošiūnaitė <greta.lukosiunaite@outlook.com>
2023-07-07 09:28:38 +0300

    Deleted commentars from styles.ts files

--- src/components/organisms/IndividualActivityTaxCalculator/styles.ts
+++ src/components/organisms/IndividualActivityTaxCalculator/styles.ts
@@ -14,12 +14,10 @@ export const SyledRadioWithLabel = styled.div`
   display: flex;
   flex-direction: column;
   gap: 0.25em;
-  /* gap: 1.5em; */
 `;
 
 export const SyledRadioWrapper = styled.div`
   display: flex;
-  /* flex-direction: column; */
   gap: 1.5rem;
 `;
 
@@ -31,11 +29,10 @@ export const StyledTitleRow = styled.div`
   display: flex;
   width: 100%;
   justify-content: space-between;
-  background-color: rgb(62, 142, 208);
-  color: rgb(255, 255, 255);
+  /* background-color: rgb(62, 142, 208);
+  color: rgb(255, 255, 255); */
   padding: 0.25rem;
   font-weight: 600;
-  /* border-radius: 0.25rem 0.25rem 0 0; */
 `;
 
 export const StyledLableColumn = styled.label`
